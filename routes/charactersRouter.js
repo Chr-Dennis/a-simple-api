a53edb8490916d20f8e950ce7672287345ff71f0
Christian <christiandennis67@gmail.com>
2025-12-09 08:18:33 -0500

    Implemented data server persistence

--- routes/charactersRouter.js
+++ routes/charactersRouter.js
@@ -1,30 +1,80 @@
 
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs/promises';
 
 const characterRouter = express.Router();
 
-import { tvCharacters } from '../data/tvCharacters.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const charactersFilePath = path.join(
+    __dirname,
+    '..',
+    'data',
+    'characters.json'
+);
 
-function getAllCharacters() {
-    return tvCharacters;
+async function getAllCharacters() {
+    try {
+        const characterData = await fs.readFile(charactersFilePath);
+        const characters = JSON.parse(characterData);
+
+        return characters;
+    } catch (error) {
+        console.error('error', error.message);
+    }
+
 }
 
-function getCharacterById(characterId) {
-    const id = parseInt(characterId);
-    const character = tvCharacters.find((character) => character.id === id);
+async function getCharacterById(characterId) {
+    try {
+        const id = parseInt(characterId);
 
-    return character;
+        const characterData = await fs.readFile(charactersFilePath);
+        const characters = JSON.parse(characterData);
+        const character = characters.find((character) => character.id === id);
+    
+        return character;
+    } catch (error) {
+        console.error ('error', error.message);
+    }
 }
 
-characterRouter.get('/', (req, res) => {
-    const characters = getAllCharacters();
+async function createCharacter(requestBody) {
+    try {
+        const characterData = await fs.readFile(charactersFilePath);
+        const characters = JSON.parse(characterData);
+
+        const newCharacter = {
+            id: characters.length + 1,
+            name: requestBody.name,
+            show: requestBody.show,
+        };
+        
+        if (!newCharacter.name || !newCharacter.show) {
+            return undefined;
+        }
+        
+        characters.push(newCharacter);
+        
+        await fs.writeFile(charactersFilePath, JSON.stringify(characters));
+        
+        return newCharacter;
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+characterRouter.get('/', async (req, res) => {
+    const characters = await getAllCharacters();
     res.status(200).json({
         data: characters,
     });
 });
 
-characterRouter.get('/:id', (req, res) => {
-    const character = getCharacterById(req.params.id);
+characterRouter.get('/:id', async (req, res) => {
+    const character = await getCharacterById(req.params.id);
 
     if (!character) {
         return res.status(404).json({
@@ -37,39 +87,30 @@ characterRouter.get('/:id', (req, res) => {
     });
 });
 
-function createCharacter(requestBody) {
-    const newCharacter = {
-        id: tvCharacters.length + 1,
-        name: requestBody.name,
-        show: requestBody.show,
-    };
 
-    if (!newCharacter.name || !newCharacter.show) {
-        return undefined;
-    }
 
-    tvCharacters.push(newCharacter)
-    return newCharacter;
-}
-
-characterRouter.post('/', (req, res) => {
-    if (!req.body) {
-        return res.status(400).json({
-            data: 'Bad Request. Missing required information',
+characterRouter.post('/', async (req, res) => {
+    try {
+        if (!req.body) {
+            return res.status(400).json({
+                data: 'Bad Request. Missing required information',
+            });
+        }
+        
+        const newCharacter = await createCharacter(req.body);
+        
+        if (!newCharacter) {
+            return res.status(400).json({
+                data: 'Bad Request. Missing required information',
+            });
+        }
+        
+        res.status(201).json({
+            data: newCharacter,
         });
+    } catch (error) {
+        console.error('error', error.message);
     }
-
-    const newCharacter = createCharacter(req.body);
-
-    if (!newCharacter) {
-        return res.status(400).json({
-            data: 'Bad Request. Missing required information',
-        });
-    }
-
-    res.status(201).json({
-        data: newCharacter,
-    });
 });
 
 export default characterRouter;
